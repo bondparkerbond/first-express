5535fc6ef5083a3f3b02e09bf9a604e7761ac6b3
bondparkerbond <parkerbond@gmail.com>
2016-03-14 14:24:56 -0600

    working products index/show pages

---
+++ index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var fs = require('fs');
+var ejs = require('ejs');
+var app = express();
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/index.html');
+});
+
+app.get('/products', function(request, response) {
+  fs.readFile('products.json', 'utf8', function(err, data) {
+    var obj = JSON.parse(data);
+    response.locals = { data: obj };
+    response.render('products.ejs');
+  });
+});
+
+app.get('/products/:id', function(request, response) {
+  fs.readFile('products.json', 'utf8', function(err, data) {
+    var products = JSON.parse(data);
+    var product = products.products.filter(function(obj) {
+        return obj.id == request.params.id;
+      });
+    if (product.length) {
+      product = product[0];
+    } else {
+      product = null;
+    }
+    response.locals = { product: product };
+    response.render('product.ejs');
+  });
+});
+
+
+
+app.listen(8080);
